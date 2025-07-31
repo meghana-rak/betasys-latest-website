48b1bf6d75ebf113ea67adc8af158e537f518244
Sejalmohitkar <sejalmohitkar2002@gmail.com>
2025-07-31 18:06:24 +0530

    fix: enhance styles for BlogCard, BlogGrid, and Sidebar components for better aesthetics and readability

--- src/components/BlogPages/BlogCard.tsx
+++ src/components/BlogPages/BlogCard.tsx
@@ -9,28 +9,28 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ date, image, category, title }) => {
   return (
-    <article className="overflow-hidden transition-shadow bg-white rounded-lg shadow-sm hover:shadow-md">
+    <article className=" bg-white rounded-lg">
       {/* Image Section */}
       <div className="relative">
         <img
           src={image}
           alt={title}
-          className="object-cover w-full h-40 rounded-t-lg sm:h-48 md:h-40 lg:h-46 xl:h-46"
+          className="object-cover w-full h-40 rounded-lg sm:h-48 md:h-40 lg:h-46 xl:h-46"
         />
         {/* Date Badge */}
         <div className="absolute top-2 left-2">
-          <span className="px-3 py-1 text-[10px] sm:text-xs font-semibold text-[#005F2B] bg-[#C4E1D1] rounded-full">
+          <span className="px-2 py-1 text-[12px] font-semibold text-[#005F2B] bg-[#C4E1D1] rounded-full">
             {date}
           </span>
         </div>
       </div>
 
       {/* Content Section */}
-      <div className="p-3 sm:p-4">
-        <p className="text-[10px] sm:text-sm font-medium uppercase text-[#00A148] mb-1">
+      <div className="py-1 sm:py-2">
+        <p className="text-[#00A148] mb-1 text-[13px] font-poppins">
           {category}
         </p>
-        <h3 className="text-sm font-poppins sm:text-base font-semibold text-[#373737] leading-snug line-clamp-2">
+        <h3 className="text-[14px] font-helvetica text-[#373737] group-hover:text-green-500 transition-colors line-clamp-2">
           {title}
         </h3>
       </div>
